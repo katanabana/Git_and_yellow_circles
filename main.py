e05af112b836396c13ea173b76896df4ec962b91
student <student@gmail.com>
2022-11-25 20:16:14 +0500

    second_commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
-import sqlite3
 import sys
 
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import *
+from UI import Ui_Form
 
 
 class Canvas(QWidget):
@@ -16,7 +16,6 @@ class Canvas(QWidget):
         if self.pb_clicked:
             p = QPainter()
             p.begin(self)
-            p.setBrush(QColor('yellow'))
             w, h = self.width(), self.height()
             a = max(min(w, h) // 2, 5)
             n = random.randint(1, 10)
@@ -24,18 +23,20 @@ class Canvas(QWidget):
                 d = random.randint(5, a)
                 x = random.randint(0, w - d)
                 y = random.randint(0, h - d)
+                rgb = [random.randint(0, 255) for i in range(3)]
+                p.setBrush(QColor(*rgb))
                 p.drawEllipse(x, y, d, d)
             p.end()
             self.pb_clicked = False
 
 
-class MyWidget(QWidget):
+class MyWidget(QWidget, Ui_Form):
     def __init__(self):
         super(MyWidget, self).__init__()
         self.initUI()
 
     def initUI(self):
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.canvas = Canvas()
         self.verticalLayout.addWidget(self.canvas)
         self.pushButton.clicked.connect(self.repaint_canvas)
